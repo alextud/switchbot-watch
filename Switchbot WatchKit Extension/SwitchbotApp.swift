9ca481c41821f0dfe70f3fea2bd0d4f21152dc7b
Alexandru Tudose <alex.tudose@tapptitude.com>
2020-09-17 21:47:16 +0300

    Initial Commit

---
+++ Switchbot WatchKit Extension/SwitchbotApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwitchbotApp.swift
+//  Switchbot WatchKit Extension
+//
+//  Created by Alexandru Tudose on 17.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwitchbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
