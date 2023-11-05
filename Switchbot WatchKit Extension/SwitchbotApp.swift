d5717b80d80297438460aa91ba685df0b8a5920d
Alexandru Tudose <alextud@gmail.com>
2023-11-05 15:49:40 +0200

    Add a timer to stop scanning when app goes to background.

--- Switchbot WatchKit Extension/SwitchbotApp.swift
+++ Switchbot WatchKit Extension/SwitchbotApp.swift
@@ -11,6 +11,7 @@ import UIKit
 @main
 struct SwitchbotApp: App {
     @ObservedObject var bluetooth = BluetoothController()
+    @State private var backgroundStopScanTimer: Timer?
     
     init() {
         scanFor(seconds: 10)
@@ -24,22 +25,25 @@ struct SwitchbotApp: App {
                         ProgressView().frame(maxWidth: 40, maxHeight: 10)
                         Text("Scanning...")
                     }
-                }
-                ForEach(bluetooth.bots, id: \.id) { bot in
-                    SwithcbotView(bot: bot)
-                }
-                if !bluetooth.isLoading {
-                    if (bluetooth.bots.count == 0) {
-                        Text("No bots found")
-                    }
+                } else {
                     Button("Reload") {
                         scanFor(seconds: 10)
                     }.foregroundColor(.blue)
                 }
+                ForEach(bluetooth.bots, id: \.id) { bot in
+                    SwithcbotView(bot: bot)
+                }
+                if !bluetooth.isLoading && bluetooth.bots.count == 0 {
+                    Text("No bots found")
+                }
             }.onReceive(NotificationCenter.default.publisher(for: WKExtension.applicationDidBecomeActiveNotification), perform: { _ in
+                self.backgroundStopScanTimer?.invalidate()
+                self.backgroundStopScanTimer = nil
                 scanFor(seconds: 10)
             }).onReceive(NotificationCenter.default.publisher(for: WKExtension.applicationWillResignActiveNotification), perform: { _ in
-                bluetooth.stopScan()
+                self.backgroundStopScanTimer = Timer.scheduledTimer(withTimeInterval: 15, repeats: false) { _ in
+                    bluetooth.stopScan()
+                }
             })
         }
     }
